8d535f2049272c42bba895c6159e520750a2e074
badmosh-anirban <unrulableanirban@gmail.com>
2026-03-22 13:16:45 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# ESP32C3 Weather Display
+
+A mini weather station using ESP32-C3, an I2C OLED display, and [OpenWeatherMap API](https://openweathermap.org/current?collection=current_forecast). The Esp32 fetches weather data from OpenWeatherMap based on your location(specified in the code) and time(from ntp), and displays information such as temperature, humidity, wind, and sunrise/sunset times etc. on the OLED, cycling through multiple screens automatically or with touch input.
+
+<img src="demo_screens.jpg" alt="demo screens" width="40%"/>
+
+## Components List
+
+- ESP32-C3 super mini (any esp32 should work)
+- SSD1306 128x64 OLED display (I2C)
+- TTP223 Touch sensor (or a push button GPIO 6)
+- Breadboard & jumper cables(optional)
+- A 5v passive buzzer (for future)
+
+## Circuit Diagram
+
+<img src="circuit_dia1.png" alt="diagram" width="60%"/>
+
+Diagram created using [Cirkit Designer](https://app.cirkitdesigner.com/)
+
+## Assembly Instructions
+
+1. Connect the OLED display to the ESP32-C3 super mini via I2C (SDA to GPIO 8, SCL to GPIO 9, or as per your board's pinout, remember to uncomment `Wire.begin(8, 9);` and specify the pins if you use so).
+2. Connect the touch sensor to GPIO 6 (or change the pin in the code).
+3. Connect the passive buzzer to GPIO 3 (or change the pin in the code). NOTE: the buzzer is not implemented as of now.
+4. Obtain an OpenWeatherMap API key by signing up at https://home.openweathermap.org/api_keys and create a new API key.
+5. Find your latitude and longitude using a service like https://www.latlong.net/ or by searching "latitude and longitude of [your location]" on Google.
+6. Connect the ESP32-C3 via USB to your computer.
+7. Upload the `esp32c3weatherv1.ino` code after entering your `ssid`, `password`, `lat`, `lon`, `apiKey`, `gmtOffset_sec` in the code.
+8. Open the Serial Monitor to see debug messages and ensure the ESP32 is connecting to WiFi and fetching weather data successfully.
+
+## Usage
+
+- When the esp32 is turned on, it tries to connect to wifi, once connected it prints the local ip address on the display for a moment.
+  <img src="wifi_screen.png" alt="diagram" width="60%"/>
+
+- Then the display will cycle through location(location is auto determined by the latitude and longitude you specify, you may also [refer here](https://openweathermap.org/api/geocoding-api?collection=other) for more information), temperature, humidity, pressure, cloudiness, visibility, wind speed, wind direction, precipitation, and sunrise/sunset time in a loop.
+- I've kept the time, day of the week and the date to be visible in every screen.
+- Tap the touch sensor to manually cycle screens.
+
+## Pro Tips
+
+- Use a dedicated 3.3V power supply for the OLED if you experience flickering.
+- Remove the red power led (blue onboard led too), if you want to use a battery to power it. You might need to use a [LDO](https://en.wikipedia.org/wiki/Low-dropout_regulator).
+- You may use seeed studio's XIAO ESP32C3, these XIAO boards have [battery charging capabilities built-in](https://wiki.seeedstudio.com/XIAO_ESP32C3_Getting_Started/#battery-usage), remember to change the pin numbers in the code accordingly.
+- Adjust `weatherInterval` and `screenInterval` in the code for faster/slower updates.
+
+## Possible Improvements
+
+- Add weather icons (sun, cloud, rain) using bitmaps.
+- Support for multiple locations.
+- Get and store the details from a portal(AP mode) instead of hardcoding.
+- Add forecast data (not just current weather).
+- Battery power and low-power optimizations.
+- Utilize the buzzer for weather alerts (e.g., storm warnings).
+- Utilize the touch sensor for more interactions (e.g., switch between current weather and forecast, double tap functionality, etc.).
+- Utilize the bluetooth capabilities of esp32c3.
+- Integrate with home automation systems.
+
+<!-- ```bash
+hiiiiii
+
+``` -->
+
+## Note
+
+- Replace all placeholders in the code (`ssid`, `password`, `lat`, `lon`, `apiKey`, `gmtOffset_sec`) with your actual values.
+- The ESP32c3 super mini has antenna design flaw, it might not connect to wifi.
+- The onboard led(Connected to the GPIO 8 (active low)) of ESP32c3 super mini may blink if you use the default I2C pins.
+- Ensure your OpenWeatherMap account is active and the API key is valid.
+- The project requires the following Arduino libraries: `WiFi.h`, `HTTPClient.h`, `ArduinoJson.h`, `Adafruit_GFX.h`, `Adafruit_SSD1306.h`, `Wire.h`.
+
+  <img src="ttp223_diagram.jpg" alt="TTP223 Touch Sensor Pinout" width="30%"/>
+
+<!-- ## Images
+
+
+
+--- -->
---
+++ esp32c3weatherv1.ino
@@ -0,0 +1,412 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <time.h>
+#include <Wire.h>
+
+// OLED declarations
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+// Replace with your wifi credentials
+const char* ssid = "replace_with_your_ssid";
+const char* password = "replace_with_your_pass";
+
+// change gpio pin for the touch switch if you want to use any other pin
+#define TOUCH_PIN 6
+
+// Replace with your coordinates
+String lat = "replace_with_your_latitude";
+String lon = "replace_with_your_longitude";
+
+// Replace with your openweathermap API key
+String apiKey = "replace_with_your_apikey";
+
+// NTP declarations, only change the gmtOffset_sec according to your country
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 19800; // +5:30
+const int daylightOffset_sec = 0;
+
+bool use24HourFormat = false; // true = 24hr, false = 12hr
+
+unsigned long weatherInterval = 180000;  // How often the Api should be called, here 3min
+const long screenInterval = 5000;   // How often should the display change, here 5 sec
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+
+// To keep track of the events
+unsigned long lastWeatherFetch = 0;
+unsigned long lastScreenChange = 0;
+unsigned long lastDisplayUpdate = 0;
+const int displayInterval = 500; // display refreshes after 
+int screenIndex = 0;
+
+// Variables to store weather info
+float temp, feels, tempMin, tempMax;
+int humidity, pressure;
+float windSpeed, windGust;
+int windDeg;
+int visibility;
+String weatherMain, weatherDesc;
+String city, country;
+int clouds;
+unsigned long sunrise;
+unsigned long sunset;
+float ifRainorSnow;
+
+// These are few icons I planned to use but did't use as of now, maybe in future, needs impovements(use bitmaps)
+void drawSun() { display.fillCircle(100, 20, 8, WHITE); }
+void drawCloud() {
+  display.fillCircle(95, 20, 6, WHITE);
+  display.fillCircle(105, 20, 6, WHITE);
+  display.fillRect(95, 20, 16, 8, WHITE);
+}
+void drawRain() {
+  drawCloud();
+  display.drawLine(98, 32, 98, 38, WHITE);
+  display.drawLine(104, 32, 104, 38, WHITE);
+}
+void drawWeatherIcon(String cond) {
+  cond.toLowerCase();
+  if (cond.indexOf("clear") >= 0) drawSun();
+  else if (cond.indexOf("rain") >= 0) drawRain();
+  else drawCloud();
+}
+
+// Function to fetch the weather info..
+void fetchWeather() {
+  HTTPClient http;
+
+  String url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat +
+               "&lon=" + lon + "&appid=" + apiKey + "&units=metric";
+
+  http.begin(url);
+  int code = http.GET();
+
+  if (code > 0) {
+    String payload = http.getString();
+    Serial.println(payload);
+
+    DynamicJsonDocument doc(4096); // its like allocating 4kb to store the json data
+    deserializeJson(doc, payload);
+    
+    // the following are the values being stored
+    temp = doc["main"]["temp"];
+    feels = doc["main"]["feels_like"];
+    tempMin = doc["main"]["temp_min"]; 
+    tempMax = doc["main"]["temp_max"]; 
+    humidity = doc["main"]["humidity"];
+    pressure = doc["main"]["pressure"];
+
+    windSpeed = doc["wind"]["speed"];
+    windDeg = doc["wind"]["deg"];
+    windSpeed = doc["wind"]["gust"];
+
+    visibility = doc["visibility"];
+    clouds = doc["clouds"]["all"];
+
+    weatherMain = doc["weather"][0]["main"].as<String>();
+    weatherDesc = doc["weather"][0]["description"].as<String>();
+
+    city = doc["name"].as<String>();
+    country = doc["sys"]["country"].as<String>();
+
+    sunrise = doc["sys"]["sunrise"];
+    sunset  = doc["sys"]["sunset"];
+    
+
+    // this is cuz always ["rain"]["1h"] or "snow"]["1h"] isn't available in the json if the location doesn't experience it
+    if(weatherMain == "Rain"){
+      ifRainorSnow = doc["rain"]["1h"];
+    }else if(weatherMain == "Snow"){
+      ifRainorSnow = doc["snow"]["1h"];
+    }else{
+      ifRainorSnow = 00.00;
+    }
+
+
+  } else {
+    Serial.println("Weather fetch failed");
+  }
+
+  http.end();
+}
+
+//old fxn i used previously
+// Function to get ntp time
+// String getTimeString() {
+//   struct tm timeinfo;
+//   if (!getLocalTime(&timeinfo)) return "Time Err";
+
+//   char buffer[20];
+
+//   if (use24HourFormat) {
+//     strftime(buffer, sizeof(buffer), "%H:%M:%S", &timeinfo);
+//   } else {
+//     strftime(buffer, sizeof(buffer), "%I:%M:%S%p", &timeinfo);
+//   }
+//   return String(buffer);
+// }
+// // Function to format ntp date
+// String getDateString() {
+//   struct tm timeinfo;
+//   if (!getLocalTime(&timeinfo)) return "Date Err";
+
+//   char buffer[15];
+//   strftime(buffer, sizeof(buffer), "%d/%m/%y", &timeinfo);
+
+//   return String(buffer);
+// }
+// // Function to get day of the week
+// String getDayString() {
+//   struct tm timeinfo;
+//   if (!getLocalTime(&timeinfo)) return "Day Err";
+
+//   char buffer[15];
+//   strftime(buffer, sizeof(buffer), "%A", &timeinfo);
+
+//   return String(buffer);
+// }
+
+
+void displayDateTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    display.setTextSize(2);
+    display.setCursor(0, 0);
+    display.println("Time Err");
+    return;
+  }
+
+  char timeBuffer[20];
+  char dateBuffer[15];
+  char dayBuffer[15];
+
+  // Time
+  if (use24HourFormat) {
+    strftime(timeBuffer, sizeof(timeBuffer), "%H:%M:%S", &timeinfo);
+  } else {
+    strftime(timeBuffer, sizeof(timeBuffer), "%I:%M:%S%p", &timeinfo);
+  }
+
+  // Date and Day
+  strftime(dateBuffer, sizeof(dateBuffer), "%d/%m/%y", &timeinfo);
+  strftime(dayBuffer, sizeof(dayBuffer), "%A", &timeinfo);
+
+  // Display
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.println(timeBuffer);
+
+  display.setTextSize(1);
+  display.print(dayBuffer);
+  display.print("    ");
+  display.println(dateBuffer);
+}
+
+// function to convert unix time into readable format
+String formatUnixTime(unsigned long unixTime) {
+  const long TIME_OFFSET = gmtOffset_sec; // +5:30 
+
+  time_t rawTime = unixTime + TIME_OFFSET;
+  struct tm *timeinfo = gmtime(&rawTime);
+
+  char buffer[20];
+
+  if (use24HourFormat) {
+    strftime(buffer, sizeof(buffer), "%H:%M", timeinfo);
+  } else {
+    strftime(buffer, sizeof(buffer), "%I:%M %p", timeinfo);
+  }
+
+  return String(buffer);
+}
+
+// basically maps the degree to corresponding direction
+String getWindDirection(int deg) {
+  const char* directions[] = {
+    "North", "NE", "East", "SE",
+    "South", "SW", "West", "NW"
+  };
+
+  int index = (deg + 22) / 45;  
+  index = index % 8;
+
+  return String(directions[index]);
+}
+
+
+// These are the screens which are being cycled upon
+void drawScreen0() {
+  // time, day, date, location
+  displayDateTime();
+
+  display.setTextSize(2);
+  display.println("");
+  display.print(city);
+
+  //display.println(weatherMain);
+  //drawWeatherIcon(weatherMain);
+}
+
+void drawScreen1() {
+  displayDateTime();
+  
+
+  display.setTextSize(2);
+  display.println("");
+  display.print(temp, 1);
+  display.println("C");
+
+  display.setTextSize(1);
+  display.print("Feels like: ");
+  display.print(feels);
+  display.println("C");
+
+  // display.print("Min: "); display.print(tempMin);
+  // display.print(" Max: "); display.println(tempMax);
+}
+
+void drawScreen2() {
+  displayDateTime();
+
+
+  display.setTextSize(1);
+  display.println("");
+
+  display.print("Humidity  : "); 
+  display.print(humidity);
+  display.println(" %");
+  display.print("Pressure  : "); 
+  display.print(pressure);
+  display.println(" hPa");
+  display.print("Cloudiness: "); 
+  display.print(clouds);
+  display.println(" %");
+  display.print("Visibility: "); 
+  display.print(visibility / 1000.0);
+  display.println(" km");
+}
+
+void drawScreen3() {
+  displayDateTime();
+
+
+  display.setTextSize(1);
+  display.println("");
+
+  display.print("Wind speed: "); 
+  display.print(windSpeed);
+  display.println(" m/s");
+  display.print("Wind Dirn : "); 
+  display.println(getWindDirection(windDeg));
+  display.print("Precip    : "); 
+  display.print(ifRainorSnow);
+  display.println(" mm/h");
+  display.println(weatherDesc);
+}
+
+void drawScreen4() {
+  displayDateTime();
+
+
+  display.setTextSize(1);
+  display.println("");
+  display.println("");
+  display.print("Sunrise: ");
+  display.println(formatUnixTime(sunrise));
+  display.print("Sunset : ");
+  display.println(formatUnixTime(sunset));
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(500); // sometimes the serial monitor fails to load
+  pinMode(TOUCH_PIN, INPUT);
+
+
+  //Wire.begin(8, 9);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("could't begin the oled display..");
+    while (true);
+  }
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+
+
+  WiFi.begin(ssid, password);
+  display.setCursor(0, 0);
+  display.println("Connecting to WiFi...");
+  display.display();
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // display in serial monitor
+  Serial.println();
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // display in oled
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("WiFi connected");
+  display.println(WiFi.localIP());
+  display.display();
+  delay(2000);  // intentionally
+  display.clearDisplay();
+
+  // NTP initialization
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+  fetchWeather(); // fetch the weather at startup
+  lastWeatherFetch = millis();
+}
+
+void loop() {
+  // Weather refreshing condition
+  if (millis() - lastWeatherFetch > weatherInterval) {
+    fetchWeather();
+    lastWeatherFetch = millis();
+  }
+
+  // Screen cycling every predefined sec
+  if (millis() - lastScreenChange > screenInterval) {
+    screenIndex = (screenIndex + 1) % 5;
+    lastScreenChange = millis();
+  }
+ 
+  // to check touch button press/tap
+  static bool lastTouchState = LOW;
+  bool currentTouchState = digitalRead(TOUCH_PIN);
+
+  if (lastTouchState == LOW && currentTouchState == HIGH) {
+    screenIndex = (screenIndex + 1) % 5;
+    lastScreenChange = millis(); // reset auto-cycle timer
+  }
+
+    lastTouchState = currentTouchState;
+
+  // To prevent the display from refreshing continuously
+  if (millis() - lastDisplayUpdate > displayInterval) {
+    lastDisplayUpdate = millis();
+
+    display.clearDisplay();
+
+    switch (screenIndex) {
+      case 0: drawScreen0(); break;
+      case 1: drawScreen1(); break;
+      case 2: drawScreen2(); break;
+      case 3: drawScreen3(); break;
+      case 4: drawScreen4(); break;
+    }
+
+    display.display();
+  }
+}
